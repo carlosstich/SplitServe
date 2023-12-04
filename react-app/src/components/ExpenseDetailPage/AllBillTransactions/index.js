4eacfbb27586fe6e7bcc2cb96c0b8b2344862327
“Carlos <carlosjstich@gmail.com>
2023-12-04 16:14:38 -0600

    delete button for transactions

--- react-app/src/components/ExpenseDetailPage/AllBillTransactions/index.js
+++ react-app/src/components/ExpenseDetailPage/AllBillTransactions/index.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React from "react";
 
-function AllBillTransactions({ userTransactions, handleOpenUpdateTransactionModal, userId }) {
+function AllBillTransactions({
+  userTransactions,
+  handleOpenUpdateTransactionModal,
+  handleDeleteTransaction,
+  userId,
+}) {
   return (
     <div className="user-transactions">
       <h3>All Bill Transactions</h3>
-      {userTransactions.map(transaction => (
+      {userTransactions.map((transaction) => (
         <div key={transaction.id} className="transaction">
           <div>Amount: ${transaction.amount.toFixed(2)}</div>
           <div>Description: {transaction.description}</div>
           <div>Status: {transaction.status}</div>
-          {transaction.sender_id === userId && !transaction.approved && transaction.status !== "Rejected" && (
-  <button
-    onClick={() => handleOpenUpdateTransactionModal(transaction)}
-    className="update-expense-button">
-    Update Transaction
-  </button>
-)}
-
+          {transaction.sender_id === userId &&
+            !transaction.approved &&
+            transaction.status !== "Rejected" && (
+              <>
+                <button
+                  onClick={() => handleOpenUpdateTransactionModal(transaction)}
+                  className="update-expense-button"
+                >
+                  Update Transaction
+                </button>
+                <button
+                  onClick={() => handleDeleteTransaction(transaction.id)}
+                  className="delete-expense-button"
+                >
+                  Delete Transaction
+                </button>
+              </>
+            )}
         </div>
       ))}
     </div>
